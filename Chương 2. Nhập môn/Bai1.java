3f07b620e7e9000f28cf6478db965d9f7d4f0e26
Anh Bui <anhbtbk@gmail.com>
2021-05-12 17:18:09 +0700

    add bai 1

---
+++ Chương 2. Nhập môn/Bai1.java
@@ -0,0 +1,16 @@
+//Nhập môn ngôn ngữ Java
+public class Bai1 {                                 //Chú ý tên class luôn bắt đầu bằng 1 chữ cái viết hoa
+    public static void main(String[] args) {        //nơi chương trình bắt đầu thực hiện, gõ tắt: psvm hoặc main
+        System.out.println("Hello World!");         //in ra màn hình, gõ tắt sout
+        System.out.println("Toi la ai");
+        System.out.println("Bui Tuan Anh");
+    }
+}
+
+/* Đây là comment
+trên nhiều dòng
+hihi
+ */
+
+//phím tắt comment 'Ctrl + /'
+//căn chỉnh lại code 'Ctrl + Alt + L'
