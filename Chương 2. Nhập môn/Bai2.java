a5c8736488d0e587da152838fa18879e2097a24d
Anh Bui <anhbtbk@gmail.com>
2021-05-13 16:57:14 +0700

    add bai 2

---
+++ Chương 2. Nhập môn/Bai2.java
@@ -0,0 +1,34 @@
+//Các kiểu dữ liệu trong Java
+public class Bai2 {
+    public static void main(String[] args) {
+        System.out.println("Gia tri min cua kieu byte: " + Byte.MIN_VALUE);
+        System.out.println("Gia tri max cua kieu byte: " + Byte.MAX_VALUE);
+        System.out.println("Kich thuoc (so bit) cua kieu byte: " + Byte.SIZE);
+        System.out.println("==================================================");
+        System.out.println("Gia tri min cua kieu char: " + Character.MIN_VALUE);
+        System.out.println("Gia tri max cua kieu char: " + Character.MAX_VALUE);
+        System.out.println("Kich thuoc (so bit) cua kieu char: " + Character.SIZE);
+        System.out.println("==================================================");
+        System.out.println("Gia tri min cua kieu short: " + Short.MIN_VALUE);
+        System.out.println("Gia tri max cua kieu short: " + Short.MAX_VALUE);
+        System.out.println("Kich thuoc (so bit) cua kieu short: " + Short.SIZE);
+        System.out.println("==================================================");
+        System.out.println("Gia tri min cua kieu int: " + Integer.MIN_VALUE);
+        System.out.println("Gia tri max cua kieu int: " + Integer.MAX_VALUE);
+        System.out.println("Kich thuoc (so bit) cua kieu int: " + Integer.SIZE);
+        System.out.println("==================================================");
+        System.out.println("Gia tri min cua kieu long: " + Long.MIN_VALUE);
+        System.out.println("Gia tri max cua kieu long: " + Long.MAX_VALUE);
+        System.out.println("Kich thuoc (so bit) cua kieu long: " + Long.SIZE);
+        System.out.println("==================================================");
+        System.out.println("Gia tri min cua kieu float: " + Float.MIN_VALUE);
+        System.out.println("Gia tri max cua kieu float: " + Float.MAX_VALUE);
+        System.out.println("Kich thuoc (so bit) cua kieu float: " + Float.SIZE);
+        System.out.println("==================================================");
+        System.out.println("Gia tri min cua kieu double: " + Double.MIN_VALUE);
+        System.out.println("Gia tri max cua kieu double: " + Double.MAX_VALUE);
+        System.out.println("Kich thuoc (so bit) cua kieu double: " + Double.SIZE);
+        System.out.println("==================================================");
+
+    }
+}
