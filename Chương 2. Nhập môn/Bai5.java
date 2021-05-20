e33c13ce1a5b71842f25a57fd0a9a0765119f35e
Anh Bui <anhbtbk@gmail.com>
2021-05-20 07:59:19 +0700

    add bai 5

---
+++ Chương 2. Nhập môn/Bai5.java
@@ -0,0 +1,42 @@
+import java.awt.desktop.AboutEvent;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+//Ép kiểu, làm tròn số
+public class Bai5 {
+    public static void main(String[] args) {
+
+//        Integer xObject = 100;
+//        int x = xObject;
+//        System.out.println(xObject.toString());
+//        System.out.println(xObject.compareTo(200));
+//
+//        System.out.println(x);
+
+//        double a = 2;
+//        double b = 3;
+//        double result = a / b;
+//        System.out.println(result);
+//        System.out.println(Math.ceil(result));
+//        System.out.println(Math.floor(result));
+//        System.out.println(Math.round(result));
+//        //sử dụng DecimalFormat
+//        NumberFormat numberFormat = new DecimalFormat("###.####");
+//        System.out.println(numberFormat.format(result));
+//        //Sử dụng printf và chuỗi chuyển đổi định dạng
+//        System.out.printf("%3.4f", result);
+//        float   x = (float)3.141592;
+//        int y = (int)x;
+//        System.out.println(y);
+//        int a = 2;
+//        int b = 3;
+//        double x = (double)a / b;
+//        System.out.println(x);
+////        System.out.printf("%.5f", x);
+//        System.out.println(Math.round(x));
+////        Integer aObject = a;
+////        System.out.println(aObject.intValue());
+        double f = 3.141592;
+        System.out.printf("%10.3f", f);
+    }
+}
