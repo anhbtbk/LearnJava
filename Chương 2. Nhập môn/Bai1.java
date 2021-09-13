28dc6f0c7d6b6a1091990d71929a68b93df34ba9
ga <anhbtbk@gmail.com>
2021-09-13 15:03:47 +0700

    Update Bai1.java

--- Chương 2. Nhập môn/Bai1.java
+++ Chương 2. Nhập môn/Bai1.java
@@ -1,6 +1,16 @@
-//Nhập môn ngôn ngữ Java
-public class Bai1 {                                 //Chú ý tên class luôn bắt đầu bằng 1 chữ cái viết hoa
-    public static void main(String[] args) {        //nơi chương trình bắt đầu thực hiện, gõ tắt: psvm hoặc main
+//Bài 1. Nhập môn ngôn ngữ Java
+
+public class Bai1 {
+    //Chú ý tên class luôn bắt đầu bằng 1 chữ cái viết hoa
+    //class tương tự như bản vẽ, đối tượng tương tự như ngôi nhà được xây dựng trên bản vẽ đó
+
+    public static void main(String[] args) {
+        //gõ tắt: psvm hoặc main
+        //phương thức main là phương thức static, kiểu void
+        //phương thức m chứa các phương thức khác, là nơi chương trình bắt đầu thực hiện
+        //static: không cần có đối tượng vẫn có thể chạy được
+        //phương thức: thể hiện hành động của đối tượng
+
         System.out.println("Hello World!");         //in ra màn hình, gõ tắt sout
         System.out.println("Toi la ai");
         System.out.println("Bui Tuan Anh");
