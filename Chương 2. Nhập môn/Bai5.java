2d1b5b0652bf38705163505ae028cb025847153d
ga <anhbtbk@gmail.com>
2021-09-15 11:13:49 +0700

    update Bai5.java

--- Chương 2. Nhập môn/Bai5.java
+++ Chương 2. Nhập môn/Bai5.java
@@ -6,12 +6,13 @@ import java.text.NumberFormat;
 public class Bai5 {
     public static void main(String[] args) {
 
-//        Integer xObject = 100;
-//        int x = xObject;
+//        Ép kiểu đóng hộp
+//        Mục đích: chuyển từ giá trị sang đối tượng để có thể sử dụng được các phương thức
+//        int x = 100;
+//        Integer xObject = x;
 //        System.out.println(xObject.toString());
-//        System.out.println(xObject.compareTo(200));
-//
-//        System.out.println(x);
+//        System.out.println(xObject.intValue());
+
 
 //        double a = 2;
 //        double b = 3;
@@ -20,23 +21,28 @@ public class Bai5 {
 //        System.out.println(Math.ceil(result));
 //        System.out.println(Math.floor(result));
 //        System.out.println(Math.round(result));
+
 //        //sử dụng DecimalFormat
-//        NumberFormat numberFormat = new DecimalFormat("###.####");
-//        System.out.println(numberFormat.format(result));
+//        NumberFormat format = new DecimalFormat("###.####");
+//        System.out.println(format.format(result));
+
 //        //Sử dụng printf và chuỗi chuyển đổi định dạng
 //        System.out.printf("%3.4f", result);
-//        float   x = (float)3.141592;
+
+//        float x = (float)3.141592;
 //        int y = (int)x;
 //        System.out.println(y);
-//        int a = 2;
-//        int b = 3;
-//        double x = (double)a / b;
-//        System.out.println(x);
-////        System.out.printf("%.5f", x);
-//        System.out.println(Math.round(x));
-////        Integer aObject = a;
-////        System.out.println(aObject.intValue());
-        double f = 3.141592;
-        System.out.printf("%10.3f", f);
+
+        int a = 2;
+        int b = 3;
+        double x = (double)a / b;
+        System.out.println(x);
+        System.out.printf("%.5f\n", x);
+        NumberFormat format = new DecimalFormat("##.####");
+        System.out.println(format.format(x));
+        System.out.println(Math.round(x));
+        Integer aObject = a;
+        System.out.println(aObject.intValue());
+
     }
 }
