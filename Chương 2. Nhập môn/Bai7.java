69483a1fbd79e37782ff03d0e99b7eac13f5c385
ga <anhbtbk@gmail.com>
2021-09-17 11:28:42 +0700

    add Bai7.java

---
+++ Chương 2. Nhập môn/Bai7.java
@@ -0,0 +1,23 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+//Bài 7. Kiểu tự suy luận
+public class Bai7 {
+    public static void main(String[] args) {
+//        var input = new Scanner(System.in);
+//        System.out.println("Enter your name: ");
+//        var fullName = input.nextLine();
+//        System.out.println("Enter your age: ");
+//        var age = input.nextInt();
+//        System.out.println("Hello " + fullName + ", you are " + age);
+//        System.out.println("fullName is " + (Object) fullName.getClass().getName());
+//        System.out.println("age is " + ((Object) age).getClass().getName());
+
+    }
+}
+
+//var : dùng khi khai báo biến cục bộ
+//biến sau khi được khai báo bằng var sẽ không thể thay đổi kiểu
+//biến cục bộ là biến được khai báo và sử dụng trong chương trình con 
