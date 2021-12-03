6504413bc5e69dcbfb43090a5c533f9556c1b9cc
ga <anhbtbk@gmail.com>
2021-12-03 17:28:33 +0700

    update Bai13ThucHanh.java

--- Chuong3_Cac_cau_truc_ra_quyet_dinh_va_vong_lap/src/Bai13ThucHanh.java
+++ Chuong3_Cac_cau_truc_ra_quyet_dinh_va_vong_lap/src/Bai13ThucHanh.java
@@ -22,12 +22,12 @@ public class Bai13ThucHanh {
 //        }
 //        System.out.println("Sum = " + sum);
 
-        //Bai 3
-        System.out.println("Nhap vao so n = : ");
-        var str = input.nextLine();
-        str = str.replaceAll("\\s+", " "); //thay nhiều khoảng cách = 1 khoảng cách
-        str = str.trim(); //xóa khoảng cách ở đầu và cuối chuỗi
-
+//        //Bai 3
+//        System.out.println("Nhap vao so n = : ");
+//        var str = input.nextLine();
+//        str = str.replaceAll("\\s+", " "); //thay nhiều khoảng cách = 1 khoảng cách
+//        str = str.trim(); //xóa khoảng cách ở đầu và cuối chuỗi
+//
 //        for (int i = 0; i <= str.length(); i++) {
 //            if (str.charAt(i) == str.matches("\\s+"") ) {
 //
@@ -35,5 +35,51 @@ public class Bai13ThucHanh {
 //            System.out.println("Sum = " + sum);
 //        }
 
+//        //Bai 4
+//        System.out.println("Nhap vao chuoi ki tu =  ");
+//        var str = input.nextLine();
+//        int count = 0;
+//        for (int i = 0; i < str.length(); i++) {
+//            count++;
+//        }
+//        System.out.println("Chuoi co " + count + " ky tu!");
+
+//        //Bai 5
+//        System.out.println("Nhap vao chuoi ki tu =  ");
+//        var str = input.nextLine();
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            str.charAt(i) =
+//        }
+//        System.out.println(str);
+
+        //Bai 6
+        System.out.println("Nhap vao so n = ");
+        var n = input.nextInt();
+        if (n <= 1) {
+            System.out.println(n + " khong la so nguyen to");
+        } else if (n == 2) {
+            System.out.println(n + " la so nguyen to");
+        } else if (n > 2) {
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    System.out.println(n + " khong la so nguyen to");
+                    break;
+                } else {
+                    System.out.println(n + " la so nguyen to");
+                }
+            }
+        }
+        //Bai 7
+        System.out.println("Nhap vao so a, b = ");
+        var a = input.nextInt();
+        var b = input.nextInt();
+
+        for (int i = a; i <= b; i++) {
+            //Phuong thuc (i)
+            sout
+
+        }
     }
 }
+
